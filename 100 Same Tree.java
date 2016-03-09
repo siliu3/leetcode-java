708903b6af17eaa6b719e3956145154eaec5a607
siliu3 <louiscityu@gmail.com>
2016-03-10 02:37:02 +0800

    Update 100 Same Tree.java

--- 100 Same Tree.java
+++ 100 Same Tree.java
@@ -9,6 +9,14 @@
  */
 public class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        
+        if(p==null||q==null){
+            return p==null&&q==null;
+        }else if(p.val!=q.val){
+            return false;
+        }else if(!isSameTree(p.left,q.left)||!isSameTree(p.right,q.right)){
+            return false;
+        }else{
+            return true;
+        }
     }
 }
